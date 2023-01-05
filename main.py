c8fef4a944f1cac65c3ec3bde55d5de48a7c06e8
Luis Quiroz <luisqpra@gmail.com>
2023-01-05 18:06:12 +0100

    path operations created

--- main.py
+++ main.py
@@ -14,10 +14,7 @@ from pydantic import Field
 
 # FastAPI
 from fastapi import FastAPI, status
-from fastapi import (
-    Body, Query, Path, Form, Header, Cookie
-    )
-from fastapi import UploadFile, File
+from fastapi import Body, Path
 from fastapi import HTTPException
 
 app = FastAPI()
@@ -132,15 +129,30 @@ class AuthorOut(AuthorBase):
     pass
 
 
-class LoginOut(BaseModel):
+class LoginBase(BaseModel):
     username: str = Field(
         ...,
         max_length=20,
         example="lita2021"
         )
-    message: str = Field(
-        default="Login Succesfully!"
+    email: EmailStr = Field(
+        ...
         )
+    message: str = Field(
+       default="Login Succesfully!"
+        )
+
+
+class Login(LoginBase):
+    password: SecretStr = Field(
+        ...,
+        min_length=8,
+        max_length=64
+    )
+
+
+class LoginOut(LoginBase):
+    pass
 
 
 # root
@@ -153,7 +165,8 @@ def home() -> Dict:
     return {"Hello": "World"}
 
 
-# Request and Response Body
+# Books
+# Create a book
 @app.post(
     path="/book/new",
     response_model=BookOut,
@@ -174,77 +187,19 @@ def create_book(book: Book = Body(...)):
     return book
 
 
-@app.post(
-    path="/author/new",
-    response_model=AuthorOut,
-    status_code=status.HTTP_201_CREATED,
-    tags=["Author"],
-    summary="Create a new author"
-    )
-def create_author(author: Author = Body(...)):
-    """
-    "Create a new author"
-
-    - Args:
-      author (Author): Author = Body(...)
-
-    - Returns:
-      The author object
-    """
-    return author
-
-
-# Validations: Query Parameters
-@app.get(
-    path="/book/details",
-    status_code=status.HTTP_200_OK,
-    tags=["Book"],
-    summary="Show details about a book"
-    )
-def show_book(
-    title: Optional[str] = Query(
-        None,
-        min_length=1,
-        max_length=100,
-        title="book title",
-        description="The name of the book",
-        example="A Monster Calls"
-        ),
-    author: Optional[str] = Query(
-        None,
-        min_length=1,
-        max_length=50,
-        title="author book",
-        description="The author of the book",
-        example="Patrick Ness"
-        )
-):
-    """
-    It returns a dictionary with the title as the key
-    and the author as the value
-
-    - Args:
-      title (Optional[str]): Optional[str] = Query(
-      author (Optional[str]): Optional[str] = Query(
-
-    - Returns:
-      A dictionary with the title and author as keys and values.
-    """
-    return {title: author}
-
-
-# Validations: Path Parameters
 books_id = [1, 2, 3, 4, 5, 6, 7, 8, 9]  # books ID
 
 
+# Show a book
 @app.get(
-    path="/book/{code_book}",
+    path="/book/{id_book}",
     status_code=status.HTTP_200_OK,
+    response_model=BookOut,
     tags=["Book"],
-    summary="Checking if a book was created"
+    summary="Show a book"
     )
 def show_book_path(
-    code_book: int = Path(
+    id_book: int = Path(
         ...,
         title="Code book",
         gt=0,
@@ -253,34 +208,59 @@ def show_book_path(
 ):
     """
     "Check a book in library"
-    It returns a dictionary with the code_book as the
-    key and a string as the value. The code_book is an
+    It returns a dictionary with the id_book as the
+    key and a string as the value. The id_book is an
     integer that is greater than 0 and the example value is 112233.
-    If the code_book is not in the code_book list, then it raises
+    If the id_book is not in the id_book list, then it raises
     an HTTPException with a status code of 404 and a detail message.
 
     - Args:
-      code_book (int): int = Path(
+      id_book (int): int = Path(
 
     - Returns:
-      A dictionary with the code_book as the key and a string as the value.
+      A dictionary with the id_book as the key and a string as the value.
     """
-    if code_book not in books_id:
+    if id_book not in books_id:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="¡This book is not in our library"
             )
-    return {code_book: "It exists!"}
+    return {id_book: "It exists!"}
 
 
+# Show all books
+@app.get(
+    path="/books",
+    response_model=List[BookOut],
+    status_code=status.HTTP_200_OK,
+    summary="Show all books",
+    tags=["Book"]
+)
+def show_all_books():
+    pass
+
+
+# Delete a book
+@app.delete(
+    path="/book/{id_book}/delete",
+    response_model=BookOut,
+    status_code=status.HTTP_200_OK,
+    summary="Delete a book",
+    tags=["Book"]
+)
+def delete_a_book():
+    pass
+
+
+# Update a book
 @app.put(
-    path="/book/update_book/{code_book}/{isbn_10}",
+    path="/book/update_book/{id_book}/{isbn_10}",
     status_code=status.HTTP_200_OK,
     tags=["Book"],
     summary="Updates a book"
     )
 def update_book(
-    code_book: int = Path(
+    id_book: int = Path(
         ...,
         title="Code book",
         gt=0,
@@ -311,154 +291,96 @@ def update_book(
     return results
 
 
-# Login -forms
+# Author
+# Create an author
+@app.post(
+    path="/author/new",
+    response_model=AuthorOut,
+    status_code=status.HTTP_201_CREATED,
+    tags=["Author"],
+    summary="Create a new author"
+    )
+def create_author(author: Author = Body(...)):
+    """
+    "Create a new author"
+
+    - Args:
+      author (Author): Author = Body(...)
+
+    - Returns:
+      The author object
+    """
+    return author
+
+
+# Show an author
+@app.get(
+    path="/author/{id_author}",
+    response_model=AuthorOut,
+    status_code=status.HTTP_200_OK,
+    summary="Show an author",
+    tags=["Author"]
+)
+def show_an_author():
+    pass
+
+
+# Show all authors
+@app.get(
+    path="/authors",
+    response_model=List[AuthorOut],
+    status_code=status.HTTP_200_OK,
+    summary="Show all authors",
+    tags=["Author"]
+)
+def show_all_authors():
+    pass
+
+
+# Delete an author
+@app.delete(
+    path="/author/{id_author}/delete",
+    response_model=AuthorOut,
+    status_code=status.HTTP_200_OK,
+    summary="Delete an author",
+    tags=["Author"]
+)
+def delete_an_author():
+    pass
+
+
+# Update an author
+@app.put(
+    path="/author/{id_author}/update",
+    response_model=AuthorOut,
+    status_code=status.HTTP_200_OK,
+    summary="Update an author",
+    tags=["Author"]
+)
+def update_an_author():
+    pass
+
+
+# Login
+# Register a user
+@app.post(
+    path="/signup",
+    response_model=LoginOut,
+    status_code=status.HTTP_201_CREATED,
+    summary="Register a User",
+    tags=["Login"]
+)
+def signup():
+    pass
+
+
+# Login a user
 @app.post(
     path="/login",
     response_model=LoginOut,
     status_code=status.HTTP_200_OK,
-    tags=["User"],
-    summary="Loging a user"
+    summary="Login a User",
+    tags=["Login"]
 )
-def login(
-    username: str = Form(
-        ...,
-        example="luisquiroz"
-        ),
-    password: SecretStr = Form(
-        ...,
-        min_length=8,
-        max_length=64,
-        example="HolaMundo"
-        )):
-    """
-    "Logs in a user and returns a success message"
-
-    - Args:
-        username (str): The username of the user.
-        password (SecretStr): The password of the user.
-
-    - Returns:
-        LoginOut: A `LoginOut` object with the following attributes:
-            username (str): The username of the user.
-            message (str): A message indicating that the login was successful.
-    """
-    return LoginOut(username=username)
-
-
-# Cookies and headers parameters
-@app.post(
-    path="/contact",
-    status_code=status.HTTP_200_OK,
-    tags=["User"],
-    summary="Form about a contact"
-)
-def contact(
-    first_name: str = Form(
-        ...,
-        max_length=20,
-        min_length=1,
-        example="Luis"
-    ),
-    last_name: str = Form(
-        ...,
-        max_length=20,
-        min_length=1,
-        example="Quiroz Prada"
-    ),
-    email: EmailStr = Form(
-        ...,
-        example="luis@yahoo.com"
-        ),
-    message: str = Form(
-        ...,
-        min_length=20,
-        example="Heyyy you, how are you?, what's going on"
-    ),
-    user_agent: Optional[str] = Header(default=None),
-    ads: Optional[str] = Cookie(
-        default=None,
-        example="this is the info that is tracking"
-        )
-):
-    """
-    "Receive and returns information about a form"
-
-    - Args:
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
-        email (EmailStr): The email of the user.
-        message (str): The message to be sent.
-        user_agent (Optional[str]): The user agent of the request.
-        ads (Optional[str]): The ads tracking information of the request.
-
-    - Returns:
-        dict: A dictionary with the following keys:
-            "header" (str): The user agent of the request.
-            "cookie" (str): The ads tracking information of the request.
-    """
-    result = {
-        "header": user_agent,
-        "cookie": ads
-    }
-    return result
-    # return user_agent
-
-
-# Files
-
-@app.post(
-    path="/post-image",
-    tags=["File"],
-    summary="Loads a image",
-    deprecated=True
-)
-def post_image(
-    image: UploadFile = File(...)
-):
-    """
-    "Uploads an image and returns information about it"
-
-    - Args:
-        image (UploadFile): The image file to be uploaded.
-
-    - Returns:
-        dict: A dictionary with the following keys:
-            "Filename" (str): The name of the image file.
-            "Format" (str): The content type of the image file.
-            "Size(kb)" (float): The size of the image file in kilobytes.
-    """
-    return {
-        "Filename": image.filename,
-        "Format": image.content_type,
-        "Size(kb)": round(len(image.file.read())/1024, ndigits=2)
-    }
-
-
-@app.post(
-    path='/post-multimages',
-    tags=["File"],
-    summary="Load several files"
-)
-def post_multimages(
-    images: List[UploadFile] = File(...)
-):
-    """
-    "Uploads several images and returns information about them"
-
-    It takes a list of images, and returns a list of
-    dictionaries containing the filename, format, and
-    size of each image
-
-    - Args:
-      images (List[UploadFile]): List[UploadFile] = File(...)
-
-    - Returns:
-      A list of dictionaries.
-    """
-    info_images = [{
-        "filename": image.filename,
-        "Format": image.content_type,
-        "Size(kb)": round(len(image.file.read())/1024, ndigits=2)
-    } for image in images]
-
-    return info_images
+def login():
+    pass
